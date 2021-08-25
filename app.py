1638f4f6999237e3ccd3034e90cb57038f96a233
Seo <voqov616@gmail.com>
2021-08-25 22:03:17 +0900

    inline 옵션 삭제

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ async def on_message(message):
 
     if message.content.startswith('$모하지'):
         embed=discord.Embed(title="굴려굴려 모하지 룰-렛", description="오늘도 무얼 해야할지 고민인 당신을 위해 제가 골라왔어요!")
-        embed.add_field(name="청마도사 1렙업하기", value="가 나왔어요!", inline=True)
+        embed.add_field(name="청마도사 1렙업하기", value="가 나왔어요!")
         embed.set_footer(text="엥? 이미 해버린거라구요? 그럼 다시 굴려굴려 모하지 룰-렛")
         await message.channel.send(embed=embed)
 
