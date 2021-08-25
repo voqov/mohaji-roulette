298e1c476ef458afb0e9c7c01ee13ff291d3c492
Seo <voqov616@gmail.com>
2021-08-25 21:45:46 +0900

    add. 모하지에 반응하기

--- app.py
+++ app.py
@@ -15,4 +15,10 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
+    if message.content.startswith('#모하지'):
+        embed=discord.Embed(title="굴려굴려 모하지 룰렛", description="삥-뽕- >>청마도사 1렙업하기<<입니다", color=0x00aaaa)
+        # embed.set_author(name="작성자의 이름",icon_url=message.author.avatar_url)
+        embed.set_footer(text="엥? 이미 해버린거라구요? 그럼 다시 굴려굴려 모하지 룰렛")
+        await message.channel.send(embed=embed)
+
 client.run(os.environ['token'])
