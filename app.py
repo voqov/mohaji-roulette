d4ce4f4369bb4edd01931373c6748ce18d0484f8
Seo <voqov616@gmail.com>
2021-08-27 18:05:01 +0900

    embed color 변경

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ async def on_message(message):
         await message.channel.send('Hello!')
 
     if message.content.startswith('$모하지'):
-        embed=discord.Embed(title="굴려굴려 모하지 룰-렛", description="오늘도 무얼 해야할지 고민인 당신을 위해 제가 골라왔어요!")
+        embed=discord.Embed(title="굴려굴려 모하지 룰-렛", description="오늘도 무얼 해야할지 고민인 당신을 위해 제가 골라왔어요!", color=0x38d7ff)
         embed.add_field(name="청마도사 1렙업하기", value="가 나왔어요!")
         embed.set_footer(text="엥? 이미 해버린거라구요? 그럼 다시 굴려굴려 모하지 룰-렛")
         await message.channel.send(embed=embed)
