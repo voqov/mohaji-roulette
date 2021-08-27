0eae9d94c3eb97a6ff0ef4b7b2707011dba4123b
Seo <voqov616@gmail.com>
2021-08-27 18:39:44 +0900

    fix. slash command

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from discord_slash import SlashCommand
 
 client = discord.Client()
-slash = SlashCommand(client)
+slash = SlashCommand(client, sync_commands=True)
 
 @client.event
 async def on_ready():
@@ -23,11 +23,18 @@ async def on_message(message):
         embed.set_footer(text="엥? 이미 해버린거라구요? 그럼 다시 굴려굴려 모하지 룰-렛")
         await message.channel.send(embed=embed)
 
+
+guild_ids = [518373344072957952]
+
 @slash.slash(
     name="모하지",
-    description="무얼할지 못 정한 당신을 위해 룰-렛을 굴려드립니다."
+    description="무얼할지 못 정한 당신을 위해 룰-렛을 굴려드립니다.",
+    guild_ids=guild_ids
 )
-async def command_mohaji():
-    pass
+async def _mohaji(ctx):
+    embed=discord.Embed(title="굴려굴려 모하지 룰-렛", description="오늘도 무얼 해야할지 고민인 당신을 위해 제가 골라왔어요!", color=0x38d7ff)
+    embed.add_field(name="청마도사 1렙업하기", value="가 나왔어요!")
+    embed.set_footer(text="엥? 이미 해버린거라구요? 그럼 다시 굴려굴려 모하지 룰-렛")
+    await ctx.channel.send(embed=embed)
 
 client.run(os.environ['token'])
