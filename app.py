4eacc46b44943a68dc6c86ce2337fa1db5a22dad
Seo <voqov616@gmail.com>
2021-08-27 19:39:16 +0900

    fix. 할일 리스트

--- app.py
+++ app.py
@@ -5,36 +5,35 @@ import random
 
 client = discord.Client()
 slash = SlashCommand(client, sync_commands=True)
-list = ["고된무기: 제타",
-"고된무기: 아니마",
-"고된무기: 에우레카",
-"고된무기: 레지스탕스",
-"디아뎀 채집물 500개",
-"쿠로수첩",
-"총당님",
-"용작",
-"트트카드",
-"골드소서 일주",
-"인벤토리 정리",
-"군힐드",
-"재생일반",
-"고된도구"]
+list = [
+        "<고대 무기: 제타> 한 단계 완성",
+        "<고대 무기: 아니마> 한 단계 완성",
+        "<고대 무기: 에우레카> 한 단계 완성",
+        "<고대 무기: 레지스탕스> 한 단계 완성",
+        "<디아뎀> 500개 채집",
+        "<쿠로의 공상 수첩> 4개 완료",
+        "<총당님> 1클",
+        "<탈 것: 용> 2클",
+        "<트리플 트라이어드> 승부해서 1장 얻기",
+        "<공략 수첩: 골드 소서> 2개 완료",
+        "인벤토리 정리",
+        "<군힐드의 사원> 1클",
+        "<남부 보즈야 전선> 돌발 교전 3클",
+        "<에덴의 낙원: 재생> 일반 파밍",
+        "<고대 도구> 한 직업 완성"
+    ]
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-guild_ids = [518373344072957952]
-
 @slash.slash(
     name="모하지",
-    description="무얼할지 못 정한 당신을 위해 룰-렛을 굴려드립니다.",
-    guild_ids=guild_ids
+    description="무얼할지 못 정한 당신을 위해 룰-렛을 굴려드립니다."
 )
 async def command_mohaji(ctx):
     embed=discord.Embed(title=random.choice(list), description="자, 어서 하러 갑시다!", color=0x38d7ff)
     embed.set_author(name=ctx.author.name + "님이 해야할 일은", icon_url=ctx.author.avatar_url)
-    embed.add_field(name=random.choice(list), value="자, 어서 하러 갑시다!")
     embed.set_footer(text="엥? 이미 해버린거라구요? 그럼 다시 `/모하지`")
     await ctx.send(embed=embed)
 
