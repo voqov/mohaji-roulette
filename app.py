2bcc9cb7fac39bbfe82c4d052f6c32910da631db
Seo <voqov616@gmail.com>
2021-08-27 18:49:03 +0900

    fix.

--- app.py
+++ app.py
@@ -9,21 +9,6 @@ slash = SlashCommand(client, sync_commands=True)
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-    if message.content.startswith('$모하지'):
-        embed=discord.Embed(title="굴려굴려 모하지 룰-렛", description="오늘도 무얼 해야할지 고민인 당신을 위해 제가 골라왔어요!", color=0x38d7ff)
-        embed.add_field(name="청마도사 1렙업하기", value="가 나왔어요!")
-        embed.set_footer(text="엥? 이미 해버린거라구요? 그럼 다시 굴려굴려 모하지 룰-렛")
-        await message.channel.send(embed=embed)
-
-
 guild_ids = [518373344072957952]
 
 @slash.slash(
@@ -35,6 +20,6 @@ async def command_mohaji(ctx):
     embed=discord.Embed(title="굴려굴려 모하지 룰-렛", description="오늘도 무얼 해야할지 고민인 당신을 위해 제가 골라왔어요!", color=0x38d7ff)
     embed.add_field(name="청마도사 1렙업하기", value="가 나왔어요!")
     embed.set_footer(text="엥? 이미 해버린거라구요? 그럼 다시 굴려굴려 모하지 룰-렛")
-    await ctx.channel.send(embed=embed)
+    await ctx.send(embed=embed)
 
 client.run(os.environ['token'])
