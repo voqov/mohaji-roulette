040492d41ca62c9c87b0880d1d7a938e96950bf9
Seo <voqov616@gmail.com>
2021-08-27 19:05:50 +0900

    add. 랜덤으로 해야할 일 고르기 기능

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 import discord
 import os
 from discord_slash import SlashCommand
+import random
 
 client = discord.Client()
 slash = SlashCommand(client, sync_commands=True)
+list = ["고된무기: 제타",
+"고된무기: 아니마",
+"고된무기: 에우레카",
+"고된무기: 레지스탕스",
+"디아뎀 채집물 500개",
+"쿠로수첩",
+"총당님",
+"용작",
+"트트카드",
+"골드소서 일주",
+"인벤토리 정리",
+"군힐드",
+"재생일반",
+"고된도구"]
 
 @client.event
 async def on_ready():
@@ -18,7 +33,7 @@ guild_ids = [518373344072957952]
 )
 async def command_mohaji(ctx):
     embed=discord.Embed(title="굴려굴려 모하지 룰-렛", description="오늘도 무얼 해야할지 고민인 당신을 위해 제가 골라왔어요!", color=0x38d7ff)
-    embed.add_field(name="청마도사 1렙업하기", value="가 나왔어요!")
+    embed.add_field(name=random.choice(list), value="가 나왔어요!")
     embed.set_footer(text="엥? 이미 해버린거라구요? 그럼 다시 굴려굴려 모하지 룰-렛")
     await ctx.send(embed=embed)
 
